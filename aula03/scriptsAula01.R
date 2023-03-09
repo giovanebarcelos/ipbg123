bce33548d42b4afccdf2d425360cdad4ff285d06
¨Giovane <giovanebarcelos@gmail.com>
2023-03-09 12:17:12 +0000

    Exemplos iniciais R

---
+++ aula03/scriptsAula01.R
@@ -0,0 +1,18 @@
+installed.packages()
+install.packages('ggplot2')
+??ggplot2
+data(cars)
+print(cars)
+
+2*3
+7/2
+7%%2
+
+plot(dnorm, from = -10, to = 10)
+x <- 1:10
+y <- x^2
+plot(x,y)
+
+w <- 13
+13 -> y
+z = 13
